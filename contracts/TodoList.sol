0d5876aeb8c493c9dfc6a0ea341f5f1e8f9fb6d3
Salman A <salmana.ar@gmail.com>
2021-04-06 16:45:26 +0530

    4. Toggling added!

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -17,6 +17,11 @@ contract TodoList {
         bool completed
     );
 
+    event TaskCompleted(
+        uint id,
+        bool completed
+    );
+
     constructor() public {
         createTask("Test!");
     }
@@ -27,4 +32,10 @@ contract TodoList {
         emit TaskCreated(taskCount, _content, false);
     }
 
+    function toggleCompleted(uint _id) public {
+        Task memory _task = tasks[_id];
+        _task.completed = !_task.completed;
+        tasks[_id] = _task;
+        emit TaskCompleted(_id, _task.completed);
+    }
 }
--- test/TodoList.test.js
+++ test/TodoList.test.js
@@ -4,6 +4,7 @@ contract('TodoList', (accounts) => {
     before(async () => {
         this.todoList = await TodoList.deployed()
     })
+    
     it('deployed successfully', async () => {
         const address = await this.todoList.address
         assert.notEqual(address, 0x0)
@@ -11,6 +12,7 @@ contract('TodoList', (accounts) => {
         assert.notEqual(address, null)
         assert.notEqual(address, undefined)
     })
+
     it('lists tasks', async () => {
         const taskCount = await this.todoList.taskCount()
         const task = await this.todoList.tasks(taskCount)
@@ -19,6 +21,7 @@ contract('TodoList', (accounts) => {
         assert.equal(task.completed, false)
         assert.equal(taskCount.toNumber(), 1)
     })
+
     it('creates tasks', async () => {
         const result = await this.todoList.createTask('A new task')
         const taskCount = await this.todoList.taskCount()
@@ -27,5 +30,14 @@ contract('TodoList', (accounts) => {
         assert.equal(event.id.toNumber(), 2)
         assert.equal(event.content, 'A new task')
         assert.equal(event.completed, false)
+    })
+
+    it('toggles task completion', async () => {
+        const result = await this.todoList.toggleCompleted(1)
+        const task = await this.todoList.tasks(1)
+        assert.equal(task.completed, true)
+        const event = result.logs[0].args
+        assert.equal(event.id.toNumber(), 1)
+        assert.equal(event.completed, true)
     })
 })
